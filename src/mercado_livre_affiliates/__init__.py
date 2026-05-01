8edc43896d61a5339235e6fcfd7cec27314989c4
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2026-05-01 19:45:50 -0300

    __all__

--- src/mercado_livre_affiliates/__init__.py
+++ src/mercado_livre_affiliates/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: 2026-present U.N. Owen <void@some.where>
 #
 # SPDX-License-Identifier: MIT
+
+from core import MercadoLivreAffiliates
+
+
+__all__ = ["MercadoLivreAffiliates"]
